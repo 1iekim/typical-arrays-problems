9516470738feee385a10cb9b82018f78b1d8fc53
1iekim <mikeil98@mail.ru>
2021-03-13 22:05:03 +0300

    feat: add check 0

--- src/index.js
+++ src/index.js
@@ -1,18 +1,24 @@
 
 exports.min = function min (array) {
+  if (typeof array === 'undefined' || array.length == 0) return 0;
+  else {
   let minVal = array[0];
   for (let i = 1; i < array.length; i++) if (array[i] < minVal) minVal = array[i];
-  return minVal;
+  return minVal;}
 }
 
 exports.max = function max (array) {
+  if (typeof array === 'undefined' || array.length == 0) return 0;
+  else {
   let maxVal = array[0];
   for (let i = 1; i < array.length; i++) if (array[i] > maxVal) maxVal = array[i];
-  return maxVal;
+  return maxVal;}
 }
 
 exports.avg = function avg (array) {
+  if (typeof array === 'undefined' || array.length == 0) return 0;
+  else {
   let avgVal = 0;
   for (let i = 0; i < array.length; i++) avgVal += array[i];
-  return avgVal/array.length;
+  return avgVal/array.length;}
 }
