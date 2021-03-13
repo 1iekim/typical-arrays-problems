75ed648219dfd15a4514ea004d3b1b1f0f23c16c
1iekim <mikeil98@mail.ru>
2021-03-13 21:08:12 +0300

    feat: become full functions

--- src/index.js
+++ src/index.js
@@ -1,12 +1,18 @@
 
 exports.min = function min (array) {
-  return 0;
+  let minVal = array[0];
+  for (let i = 1; i < array.length; i++) if (array[i] < minVal) minVal = array[i];
+  return minVal;
 }
 
 exports.max = function max (array) {
-  return 0;
+  let maxVal = array[0];
+  for (let i = 1; i < array.length; i++) if (array[i] > maxVal) maxVal = array[i];
+  return maxVal;
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  let avgVal = 0;
+  for (let i = 0; i < array.length; i++) avgVal += array[i];
+  return avgVal/array.length;
 }
